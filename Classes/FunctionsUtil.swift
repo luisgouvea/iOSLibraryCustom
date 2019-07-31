fd6c3eea5814f6b9fd63b0407586c4ea1874ed1c
luisgouvea <luiseduardo.gouvea@gmail.com>
2019-07-31 14:28:10 -0300

    hikn jbhj

---
+++ Classes/FunctionsUtil.swift
@@ -0,0 +1,15 @@
+//
+//  FunctionsUtil.swift
+//  iOSLibraryCustom
+//
+//  Created by Luis Eduardo Gouvea on 31/07/19.
+//
+
+import UIKit
+
+public class FunctionsUtil: NSObject {
+    
+    static func appendStringCustom(_ text : String) -> String {
+        return "\(text) algumacoisa"
+    }
+}
