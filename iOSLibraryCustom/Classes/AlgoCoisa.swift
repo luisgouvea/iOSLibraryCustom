4e4f3c3c07ee0577b64cb832030173b8a11c1b23
luisgouvea <luiseduardo.gouvea@gmail.com>
2019-07-31 18:18:11 -0300

    fsdf dsf

---
+++ iOSLibraryCustom/Classes/AlgoCoisa.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  iOSLibraryCustom
+//
+//  Created by Luis Eduardo Gouvea on 31/07/19.
+//
+
+import UIKit
+
+public class AlgoCoisa: NSObject {
+    static public func appendStringCustom(_ text : String) -> String {
+        return "\(text) algumacoisa"
+    }
+}
